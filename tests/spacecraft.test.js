55e4ffb23bb868f758fa345cb5a8b25b2b5bef62
vijay991 <makwanavijay8267@gmail.com>
2024-02-03 14:37:19 +0530

    added all test scenario for move forward

--- tests/spacecraft.test.js
+++ tests/spacecraft.test.js
@@ -1,10 +1,21 @@
 const Spacecraft = require('../src/spacecraft');
 
 describe('Spacecraft', () => {
-    it('should move forward correctly', () => {
-        const spacecraft = new Spacecraft(0, 0, 0, 'N');
-        spacecraft.moveForward();
-        expect(spacecraft.position).toEqual([0, 1, 0]);
-        expect(spacecraft.direction).toBe('N');
+
+    describe('Test for move forward', () => {
+        test.each([
+            ['N', [0, 1, 0], 'N'],
+            ['S', [0, -1, 0], 'S'],
+            ['E', [1, 0, 0], 'E'],
+            ['W', [-1, 0, 0], 'W'],
+            ['Up', [0, 0, 1], 'Up'],
+            ['Down', [0, 0, -1], 'Down'],
+        ])('should move forward correctly for direction %s', (initialDirection, expectedPosition, expectedDirection) => {
+            const spacecraft = new Spacecraft(0, 0, 0, initialDirection);
+            spacecraft.moveForward();
+            expect(spacecraft.position).toEqual(expectedPosition);
+            expect(spacecraft.direction).toBe(expectedDirection);
+        });
     });
+
 });

001490d802c827a1974f9c661a351ffbb451df13
vijay991 <makwanavijay8267@gmail.com>
2024-02-07 10:58:56 +0530

    refector unit test

--- tests/spacecraft.test.js
+++ tests/spacecraft.test.js
@@ -1,111 +1,50 @@
 const Spacecraft = require('../src/spacecraft');
 
 describe('Spacecraft', () => {
+    let spacecraft;
 
-    describe('Test for move forward', () => {
-        test.each([
-            ['N', [0, 1, 0], 'N'],
-            ['S', [0, -1, 0], 'S'],
-            ['E', [1, 0, 0], 'E'],
-            ['W', [-1, 0, 0], 'W'],
-            ['Up', [0, 0, 1], 'Up'],
-            ['Down', [0, 0, -1], 'Down'],
-        ])('should move forward correctly for direction %s', (initialDirection, expectedPosition, expectedDirection) => {
-            const spacecraft = new Spacecraft(0, 0, 0, initialDirection);
-            spacecraft.moveForward();
-            expect(spacecraft.position).toEqual(expectedPosition);
-            expect(spacecraft.direction).toBe(expectedDirection);
-        });
+    beforeEach(() => {
+        spacecraft = new Spacecraft(0, 0, 0, 'N');
     });
 
-    describe('Test for move backward', () => {
-        test.each([
-            ['N', [0, -1, 0], 'N'],
-            ['S', [0, 1, 0], 'S'],
-            ['E', [-1, 0, 0], 'E'],
-            ['W', [1, 0, 0], 'W'],
-            ['Up', [0, 0, -1], 'Up'],
-            ['Down', [0, 0, 1], 'Down'],
-        ])('should move backward correctly for direction %s', (initialDirection, expectedPosition, expectedDirection) => {
-            const spacecraft = new Spacecraft(0, 0, 0, initialDirection);
-            spacecraft.moveBackward();
-            expect(spacecraft.position).toEqual(expectedPosition);
-            expect(spacecraft.direction).toBe(expectedDirection);
-        });
+    test('should initialize with correct position and direction', () => {
+        expect(spacecraft.position).toEqual([0, 0, 0]);
+        expect(spacecraft.direction).toBe('N');
     });
 
-    describe('Test for turnLeft', () => {
-        test.each([
-            ['N', 'W'],
-            ['W', 'S'],
-            ['S', 'E'],
-            ['E', 'N'],
-            ['Up', 'N'],
-            ['Down', 'S']
-        ])('should turn left correctly from direction %s to %s', (initialDirection, expectedDirection) => {
-            const spacecraft = new Spacecraft(0, 0, 0, initialDirection);
-            spacecraft.turnLeft();
-            expect(spacecraft.direction).toBe(expectedDirection);
-        });
+    test('should move forward correctly', () => {
+        spacecraft.executeCommands(['f']);
+        expect(spacecraft.position).toEqual([0, 1, 0]);
     });
 
-    describe('Test for turnRight', () => {
-        test.each([
-            ['N', 'E'],
-            ['E', 'S'],
-            ['S', 'W'],
-            ['W', 'N'],
-            ['Up', 'S'],
-            ['Down', 'N']
-        ])('should turn right correctly from direction %s to %s', (initialDirection, expectedDirection) => {
-            const spacecraft = new Spacecraft(0, 0, 0, initialDirection);
-            spacecraft.turnRight();
-            expect(spacecraft.direction).toBe(expectedDirection);
-        });
+    test('should move backward correctly', () => {
+        spacecraft.executeCommands(['b']);
+        expect(spacecraft.position).toEqual([0, -1, 0]);
     });
 
-    describe('Test for turnUp', () => {
-        test.each([
-            ['N', 'Up'],
-            ['S', 'Up'],
-            ['E', 'Up'],
-            ['W', 'Up'],
-            ['Up', 'Up'],
-            ['Down', 'Down'],
-        ])('should turn up correctly for initial direction %s', (initialDirection, expectedDirection) => {
-            const spacecraft = new Spacecraft(0, 0, 0, initialDirection);
-            spacecraft.turnUp();
-            expect(spacecraft.direction).toBe(expectedDirection);
-        });
+    test('should turn left correctly', () => {
+        spacecraft.executeCommands(['l']);
+        expect(spacecraft.direction).toBe('W');
     });
 
-    describe('Test for turnDown', () => {
-        test.each([
-            ['N', 'Down'],
-            ['S', 'Down'],
-            ['E', 'Down'],
-            ['W', 'Down'],
-            ['Up', 'Up'],
-            ['Down', 'Down'],
-        ])('should turn down correctly for initial direction %s', (initialDirection, expectedDirection) => {
-            const spacecraft = new Spacecraft(0, 0, 0, initialDirection);
-            spacecraft.turnDown();
-            expect(spacecraft.direction).toBe(expectedDirection);
-        });
+    test('should turn right correctly', () => {
+        spacecraft.executeCommands(['r']);
+        expect(spacecraft.direction).toBe('E');
     });
 
-    describe('Test for executeCommands', () => {
-        test.each([
-            [['f', 'r', 'u', 'b', 'l'], [0, 1, -1], 'N'],
-            [['l', 'l', 'l', 'l'], [0, 0, 0], 'N'],
-            [['b', 'b', 'b'], [0, -3, 0], 'N'],
-            [['u', 'u', 'u'], [0, 0, 0], 'Up'],
-        ])('should execute commands correctly', (commands, expectedPosition, expectedDirection) => {
-            const spacecraft = new Spacecraft(0, 0, 0, 'N');
-            spacecraft.executeCommands(commands);
+    test('should turn up correctly', () => {
+        spacecraft.executeCommands(['u']);
+        expect(spacecraft.direction).toBe('Up');
+    });
 
-            expect(spacecraft.position).toEqual(expectedPosition);
-            expect(spacecraft.direction).toBe(expectedDirection);
-        });
+    test('should turn down correctly', () => {
+        spacecraft.executeCommands(['d']);
+        expect(spacecraft.direction).toBe('Down');
+    });
+
+    test('should execute multiple commands correctly', () => {
+        spacecraft.executeCommands(['f', 'r', 'u', 'b', 'l']);
+        expect(spacecraft.position).toEqual([0, 1, -1]);
+        expect(spacecraft.direction).toBe('N');
     });
 });
